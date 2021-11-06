1bc36f56fc3a82ab1f52b1abb1107e02f8ece34a
devphenom <iphenom01@gmail.com>
2021-11-06 10:48:29 +0100

    Added Main Section

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,5 +1,39 @@
+// libraries
 import React from "react";
+import SearchIcon from "@material-ui/icons/Search";
+import MicIcon from "@material-ui/icons/Mic";
 
+// Styles
+import useStyles from "./styles";
+import "./Main.scss";
+
+// Component
 export default function Main() {
-  return <div>Main</div>;
+  const classes = useStyles();
+
+  return (
+    <section id="main">
+      <div className="row">
+        <div>
+          <img src="../" alt="" />
+        </div>
+        <div className="searchField">
+          <SearchIcon className={classes.searchBar} />
+          <input type="text" />
+          <MicIcon color="primary" />
+        </div>
+        <div className="buttons">
+          <button className="searchBtn">Google Search</button>
+          <button className="feel lucky">I'm feeling lucky</button>
+        </div>
+        <p>
+          Google offered in{" "}
+          <a href="https://www.google.com/search?q=deutsch">Deutsch</a>,{" "}
+          <a href="https://www.google.com/search?q=italino">Italino</a>,{" "}
+          <a href="https://www.google.com/search?q=francais">Francais</a>,{" "}
+          <a href="https://www.google.com/search?q=rumantsch">Rumantsch</a>,{" "}
+        </p>
+      </div>
+    </section>
+  );
 }
---
+++ src/components/Main/styles.js
@@ -0,0 +1,6 @@
+import { makeStyles } from "@material-ui/core/styles";
+export default makeStyles(() => ({
+  searchBar: {
+    color: "#c7c7c9",
+  },
+}));
